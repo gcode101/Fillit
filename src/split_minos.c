035752d38668d877d3a3e74a9c2ff1c9c58628e5
Henry Huang <hehuang@e1z1r6p34.42.us.org>
2016-10-21 11:24:50 -0700

    initial finish for split_minos.c

--- src/split_minos.c
+++ src/split_minos.c
@@ -6,35 +6,41 @@
 /*   By: gcortina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 12:22:41 by gcortina          #+#    #+#             */
-/*   Updated: 2016/10/20 15:07:55 by gcortina         ###   ########.fr       */
+/*   Updated: 2016/10/21 11:23:58 by hehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+bool	still_in_mino(char *str, int i)
+{
+	if ((str[i] == '\n' && str[i + 1] == '\n') || str[i] == '\0')
+		return (FALSE);
+	return (TRUE);
+}
+
 char	**split_minos(char *str)
 {
 	int		i;
 	int		j;
+	int		k;
 	char	**minos;
-	char	elem;
 
 	i = 0;
-	j = 0;
+	k = 0;
 	if (!(minos = (char**)malloc(sizeof(char*) * (minos_count(str)) + 1)))
 		return (NULL);
 	while (str[i])
 	{
-		if (!(elem = malloc(sizeof(char) * (TETRIMINO) + 1)))
+		if (!(minos[k] = malloc(sizeof(char) * (TETRIMINO) + 1)))
 			return (NULL);
-		while (1)
-		{
-			elem[j++] = str[i++];
-			if (str[i] == '\n' && str[i + 1] == '\n')
-				break;
-		}
-		
-		i++;
+		j = 0;
+		while (still_in_mino(str, i))
+			minos[k][j++] = str[i++];
+		minos[k][j] = '\0';
+		k++;
+		i += 2;
 	}
+	minos[k] = NULL;
 	return (minos);
 }
